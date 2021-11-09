a216998f0e30708791fde99ee10400e4e7d691bc
haukeeckel <Haukeeckel@posteo.de>
2021-11-09 17:03:45 +0100

    finished expressjs

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+//  MIDDLEWARE
+app.use(express.static(`${__dirname}/public`));
+
+
+// ROUTES
+app.get("/", function(req, res, next) {
+  res.sendFile(`${__dirname}/views/index.html`);
+});
+
+app.get("/work", function(req, res, next) {
+  res.sendFile(`${__dirname}/views/work.html`);
+});
+
+app.get("/about", function(req, res, next) {
+  res.sendFile(`${__dirname}/views/about.html`);
+});
+
+app.listen(4000);
